87bce5f8983f5101a41e3404b8cb5e438b3a0e68
Russell Smith <russell@octopart.com>
2016-02-18 22:28:25 -0500

    Add url of auth docs

--- secrets.template.py
+++ secrets.template.py
@@ -1,3 +1,5 @@
+# https://dev.moves-app.com/docs/authentication
+
 # Go to this URL on desktop, then enter the PIN in the mobile app, then wait for the
 # desktop browser to redirect and copy code below.
 # <redacted>
