de644e08ebb27fb0b75e4c42eb28c75dfcb7299f
Russell Smith <russell@octopart.com>
2016-02-18 21:56:16 -0500

    Initial commit

---
+++ secrets.template.py
@@ -0,0 +1,9 @@
+# Go to this URL on desktop, then enter the PIN in the mobile app, then wait for the
+# desktop browser to redirect and copy code below.
+# <redacted>
+
+client_id = ''
+client_secret = ''
+code = ''
+
+access_token = ''
---
+++ test.py
@@ -0,0 +1,147 @@
+from moves import MovesClient
+import requests
+import dateutil.parser
+from pprint import pprint
+from collections import defaultdict
+
+import secrets
+
+
+def main():
+    #moves = MovesClient()
+    #access_token = moves.get_oauth_token(code)
+    #print(access_token)
+
+    #print(moves.api('/user/profile', 'GET', params={'access_token': access_token}))
+
+    #resp = requests.post('https://api.moves-app.com/oauth/v1/access_token', {
+        #'grant_type': 'authorization_code',
+        #'code': secrets.code,
+        #'client_id': secrets.client_id,
+        #'client_secret': secrets.client_secret,
+        #'redirect_uri': 'http://qqrs.github.io/'})
+    #if resp.status_code != 200:
+    #   print(resp.text)
+    #access_token = resp.json()['access_token']
+    #print access_token
+
+    moves = MovesClient()
+    #resp = moves.api('user/profile', 'GET', params={'access_token': access_token})
+
+    #resp = moves.api('user/places/daily/20160217', 'GET', params={'access_token': access_token})
+    #today_places = resp.json()[0]['segments']
+    #print([(v['place']['name'], v['startTime'], v['endTime']) for v in today_places])
+    #octopart_start_iso = [v for v in today_places if v['place']['name'] == 'Octopart.com'][0]['startTime']
+    #octopart_start_dt = dateutil.parser.parse(octopart_start_iso)
+    #print(octopart_start_dt)
+
+    resp = moves.api('user/places/daily/201602', 'GET', params={'access_token': secrets.access_token})
+
+    stats_defs = {
+        'work': {
+            'place_names': {'Octopart.com'},
+            'stats': {'hours', 'arrive_morning', 'depart_evening'}
+        },
+        'home': {
+            'place_names': {'Home'},
+            'stats': {'depart_morning'}
+        },
+        'gym': {
+            'place_names': {'24 Hour Fitness', 'Calexico Cart'},
+            'stats': {'hours'}
+        },
+    }
+
+
+    for day in resp.json():
+        #stats = extract_daily_place_work_stats(day)
+        #stats = extract_daily_place_home_stats(day)
+        stats = extract_daily_place_stats(day, stats_defs)
+        if stats:
+            pprint(stats, width=240)
+        else:
+            print('---')
+
+def extract_daily_place_work_stats(day, filter_place='Octopart.com'):
+        date = dateutil.parser.parse(day['date'])
+        if date.weekday() in (5, 6):        # Skip Sat, Sun
+            return None
+        day_places = day['segments'] or []
+        times = [
+            (dateutil.parser.parse(v['startTime']),
+             dateutil.parser.parse(v['endTime']))
+                for v in day_places if v['place'].get('name') == filter_place]
+        for v in day_places:
+            print(v['place'].get('name'))
+
+        return {
+            'date': date.strftime('%a %m-%d-%Y'),
+            # TODO: fix hours across midnight boundaries
+            'hours': '%.2f' % sum((t[1] - t[0]).total_seconds() / 3600 for t in times),
+            'times': ['%s - %s' % (t[0].strftime('%I:%M %p'), t[1].strftime('%I:%M %p')) for t in times]
+            #'times': ['%s - %s' % (str(t[0]), str(t[1])) for t in times]
+        }
+
+def extract_daily_place_home_stats(day, filter_place='Home'):
+        date = dateutil.parser.parse(day['date'])
+        if date.weekday() in (5, 6):        # Skip Sat, Sun
+            return None
+        day_places = day['segments'] or []
+        times = [
+            (dateutil.parser.parse(v['startTime']),
+             dateutil.parser.parse(v['endTime']))
+                for v in day_places if v['place'].get('name') == filter_place]
+        return {
+            'date': date.strftime('%a %m-%d-%Y'),
+            'hleft': times[0][1].strftime('%I:%M %p') if times else None,
+            #'times': ['%s - %s' % (t[0].strftime('%I:%M %p'), t[1].strftime('%I:%M %p')) for t in times]
+            'times': ['%s - %s' % (str(t[0]), str(t[1])) for t in times]
+        }
+
+
+def extract_daily_place_stats(day, defs):
+    date = dateutil.parser.parse(day['date'])
+    if date.weekday() in (5, 6):        # Skip Sat, Sun
+        return None
+    day_places = day['segments'] or []
+    place_times = defaultdict(list)
+    for v in day_places:
+        for place, d in defs.iteritems():
+            if v['place'].get('name') in d['place_names']:
+                place_times[place].append(
+                    (dateutil.parser.parse(v['startTime']),
+                     dateutil.parser.parse(v['endTime'])))
+
+    stats = {
+        'date': date.strftime('%a %m-%d-%Y'),
+    }
+
+    for place, d in defs.iteritems():
+        times = place_times[place]
+        for stat_type in d['stats']:
+            stat_name = place + '_' + stat_type
+            val = get_stat_value_from_times(stat_type, times)
+            stats[stat_name] = val or '        '
+    return stats
+
+
+def get_stat_value_from_times(stat_type, times):
+    if stat_type == 'hours':
+        # TODO: fix hours across midnight boundaries
+        return '%.2f' % sum((t[1] - t[0]).total_seconds() / 3600 for t in times)
+    elif stat_type == 'times': 
+        return ['%s - %s' % (t[0].strftime('%I:%M %p'),
+                             t[1].strftime('%I:%M %p'))
+                                for t in times]
+    elif stat_type == 'arrive_morning': 
+        return times[0][0].strftime('%I:%M %p') if times else None
+    elif stat_type == 'depart_morning': 
+        return times[0][1].strftime('%I:%M %p') if times else None
+    elif stat_type == 'depart_evening': 
+        return times[-1][1].strftime('%I:%M %p') if times else None
+    else:
+        raise Exception('Unrecognized stat_type: %s' % stat_type)
+
+
+if __name__ == '__main__':
+    main()
